34ce14e896f76bfe2f65f3c84ef5ab76347612a7
shraddharokdez-coder <shraddha.rokdez@gmail.com>
2026-01-13 14:42:03 +0530

    fundamentals of js

---
+++ Day1.js
@@ -0,0 +1,42 @@
+var name = "shraddha";
+let age = 18;
+const Birthplace = "nashik";
+
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("Birthplace:", Birthplace);
+
+
+
+function add(a,b)
+{
+  console.log(a+b)
+}
+
+add(5,10)
+
+let arr = [1,2,3,4]
+
+console.log(arr)
+
+arr.push(5)
+
+let day = 2
+
+switch(day)
+{
+  case 1 : console.log("Monday")
+  break;
+
+  case 2: console.log("Tuesday")
+  break;
+  
+  case 3: console.log("Wednesday")
+  break;
+
+  case 4: console.log("Thursday")
+  break;
+
+  default : console.log("invalid")
+  break;
+}
